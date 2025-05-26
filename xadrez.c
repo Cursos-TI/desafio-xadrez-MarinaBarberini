49808b8cb0222a4ec468354898937240877bae4e
Marina <marinabarberini3914@gmail.com>
2025-05-26 19:50:31 +0000

    segunda parte feita

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main() {
 int i;  //torre
 int j = 1;  //bispo + inicializacao
 int k = 1;  //rainha + inicializacao
+int passoHorizontal, passoVertical; //cavalo
 
 //movimentação da torre usando for
 for(i = 1; i <= 5; i++){
@@ -23,5 +24,18 @@ do{
     k++;
 } while (k <= 8);
 
+// Movimento do cavalo em "L" (duas casas para baixo + uma para esquerda)
+    
+    // loop for para duas casas para baixo
+    for (passoVertical = 1; passoVertical <= 2; passoVertical++) {
+        printf("Baixo\n");
+    }
+
+    // loop while para uma casa para esquerda
+    passoHorizontal = 1;
+    while (passoHorizontal <= 1) {
+        printf("Esquerda\n");
+        passoHorizontal++;
+    }
     return 0;
 }
